b2f4c4c56ccdbd59185d86534fba04aac2d08a5f
Kakoto <vinhlam.le29@gmail.com>
2025-12-23 22:03:20 +0100

    header update
    Ajout d'un composant header.vue qui sera utilisable sur toutes les pages.
    Modification des fichier css

---
+++ app/src/components/Header.vue
@@ -0,0 +1,40 @@
+<script setup>
+
+import { ref } from 'vue';
+
+const MenuBurger = ref(false);
+
+const ActiverMenuBurger = () => {
+    MenuBurger.value = !MenuBurger.value
+}
+
+</script>
+
+<template>
+    <header class="flex justify-center">
+        <div class="flex justify-between w-9/10">
+            <div>
+                <h1>MEMORY</h1>
+            </div>
+            <div class="flex items-center">
+                <nav>
+                    <div>
+                        <button class="block lg:hidden">Button</button>
+                    </div>
+                    <div class="hidden lg:block">
+                        <a href="">Accueil</a>
+                        <a href="">Leaderboard</a>
+                    </div>
+                </nav>
+            </div>
+        </div>
+    </header>
+</template>
+
+<style scoped>
+
+    @media (min-width: 1024px){
+
+    }
+</style>
+
