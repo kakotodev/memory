e3c3d5cc03d95aecb03f413e7afefa7fd33e2405
Kakoto <vinhlam.le29@gmail.com>
2026-01-01 00:34:49 +0100

    update

---
+++ app/src/components/SelectedMode.vue
@@ -0,0 +1,24 @@
+<script setup>
+import { defineEmits } from 'vue';
+
+defineEmits(['select-mode'])
+
+</script>
+
+<template>
+    <div class="place-items-center">
+        <h1>Sélectionner un mode de jeux</h1>
+    </div>
+    <div class="place-items-center">
+        <button @click="$emit('select-mode','Classique')">Classique</button>
+        <button @click="$emit('select-mode', 'Survie')">Survie</button>
+    </div>
+</template>
+
+<style scoped>
+    button {
+        display: flex;
+        
+        font-size: 30px;
+    }
+</style>
--- app/src/views/GameModeView.vue
+++ app/src/views/GameModeView.vue
@@ -1,41 +1,29 @@
 <script setup>
-import { RouterLink } from 'vue-router';
+import { ref } from 'vue';
+
+import SelectedMode from '@/components/SelectedMode.vue';
+
+const SelectedMode = ref(null)
+
+const updateMode = (modeSelectionner) => {
+    SelectedMode.value = modeSelectionner
+}
+
 </script>
 
 <template>
     <main>
-        <div class="pb-66">
-            <div class="place-items-center">
-                <h1>Sélectionner un </h1>
-                <h1 style="padding-left: 3rem;">mode de jeux</h1>
-            </div>
-            
-            <div class="place-items-center py-8">
-                <nav class="flex flex-col space-y-6 place-items-center">
-                    <router-link to="/" class="btn">
-                        Classique
-                    </router-link>
-                    
-                    <router-link to="/" class="btn">
-                        Survie
-                    </router-link>
-                    
-                    <router-link to="/" class="btn" >
-                        Quitter
-                    </router-link>
-                </nav>
-            </div>
+        <div id="SectionGame" class="pb-66">
+            <SelectedMode v-if="SelectedMode === null" />
         </div>
     </main>
 </template>
 
 <style scoped>
-    .btn {
-        padding: 1rem;
-        background-color: white;
-        color: black;
-        border: 1px solid black;
-        width: 16rem;
-        text-align: center;
+
+    #SectionGame {
+        border: solid;
+        width: 1800px;
+        height: 800px;
     }
 </style>
