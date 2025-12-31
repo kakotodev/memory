0ea2a835c7a7fb4d39e12966d2baa4cbdf26582e
MIchel Khauv <michelkhauv@gmail.com>
2025-12-31 16:54:21 +0100

    maj et Ajout GameModeView

--- app/src/router/index.js
+++ app/src/router/index.js
@@ -1,5 +1,11 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import AccueilView from '@/views/AccueilView.vue'
+<<<<<<< Updated upstream
+=======
+import ReglesView from '@/views/ReglesView.vue'
+import ClassementView from '@/views/ClassementView.vue'
+import GameModeView from '@/views/GameModeView.vue'
+>>>>>>> Stashed changes
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -9,6 +15,24 @@ const router = createRouter({
       name: 'accueil',
       component: AccueilView,
     },
+<<<<<<< Updated upstream
+=======
+    {
+      path: '/rules',
+      name: 'regles',
+      component: ReglesView,
+    },
+    {
+      path: '/leaderboard',
+      name: 'classement',
+      component: ClassementView
+    },
+    {
+      path: '/play',
+      name: 'play',
+      component: GameModeView
+    }
+>>>>>>> Stashed changes
   ],
 })
 
